ff26c6fa9876d769796941cbc63facf7b6f28215
egnzz <egnzz@mail.ru>
2022-06-22 22:18:42 +0300

    4-й модуль: 2 и 3 задачи

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,5 @@
 function makeDiagonalRed(table) {
-  // ваш код...
+  for (let tr of table.rows) {
+	tr.cells[tr.rowIndex].style.backgroundColor = 'red';
+  }
 }
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,24 @@
 function highlight(table) {
-  // ваш код...
+  let lastTD = table.querySelectorAll('tr > td:last-child');
+  for(let prop of lastTD) {
+   if(prop.getAttribute("data-available") == 'true') {
+	   console.log(prop.getAttribute("data-available"))
+	prop.parentElement.classList.add("available");
+   } else if(prop.getAttribute("data-available") == 'false') prop.parentElement.classList.add("unavailable");
+
+  if(!prop.hasAttribute("data-available")) {
+	  prop.parentElement.setAttribute("hidden", "");
+  }
+
+  if(prop.previousElementSibling.textContent === 'm') {
+	prop.parentElement.classList.add("male");
+   } else if (prop.previousElementSibling.textContent === 'f')
+      prop.parentElement.classList.add("female");    
+ }
+
+  let firstTD = table.querySelectorAll('tr > td:first-child');
+  for(let prop of firstTD) {
+   let age = parseInt(prop.nextElementSibling.innerHTML);
+   if(age < 18) prop.parentElement.style.textDecoration = 'line-through';
+  }
 }

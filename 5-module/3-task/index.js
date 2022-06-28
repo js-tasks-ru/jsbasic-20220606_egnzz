b8662f08b57ba3bc789eb36e250e15581ec1a518
egnzz <egnzz@mail.ru>
2022-06-28 21:26:20 +0300

    5-й модуль, 3-я задача

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,36 @@
 function initCarousel() {
-  // ваш код...
+  const maxSlides = 4;
+  const carouselMove = document.querySelector('.carousel__inner');
+  const leftArrow = document.querySelector('.carousel__arrow_left');
+  const rightArrow = document.querySelector('.carousel__arrow_right');
+
+  let activeSlide = 0;
+  arrowsVisibility();
+
+  rightArrow.addEventListener("click", () => {
+    activeSlide--;
+    carouselMove.style.transform = 'translateX(' + carouselMove.offsetWidth * activeSlide + 'px)';
+    arrowsVisibility();
+  });
+
+  leftArrow.addEventListener("click", () => {
+    activeSlide++;
+    carouselMove.style.transform = 'translateX(' + carouselMove.offsetWidth * activeSlide + 'px)';
+    arrowsVisibility();
+  });
+
+  function arrowsVisibility() {
+    if (activeSlide <= (maxSlides - 1)) {
+      rightArrow.style.display = 'none';
+      leftArrow.style.display = '';
+    }
+    if (activeSlide >= 0) {
+      rightArrow.style.display = '';
+      leftArrow.style.display = 'none';
+    }
+    if ((activeSlide < 0) && (activeSlide > -(maxSlides - 1))) {
+      rightArrow.style.display = '';
+      leftArrow.style.display = '';
+    }
+  }
 }

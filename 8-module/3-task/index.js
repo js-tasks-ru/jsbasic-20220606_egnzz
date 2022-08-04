add86a66ece6fe52b807831ff8da0af10038859e
egnzz <egnzz@mail.ru>
2022-08-04 22:22:35 +0300

    8-й модуль, 3-я задача

--- 8-module/3-task/index.js
+++ 8-module/3-task/index.js
@@ -6,23 +6,64 @@ export default class Cart {
   }
 
   addProduct(product) {
-    // ваш код
+    if(product) {
+      this.product = product;
+
+      if (this.isEmpty()) {
+        this.cartItems.push({product: this.product, count: 1});
+        this.onProductUpdate({ product: this.product, count: 1 });
+      }
+      else {
+
+        let findResult = this.cartItems.find(item => this.product.id === item.product.id); 
+        if (findResult) {
+          findResult.count++;
+          this.onProductUpdate(findResult);
+        }
+        else {
+          this.cartItems.push({ product: this.product, count: 1 });
+          this.onProductUpdate({ product: this.product, count: 1 });
+        } 
+      } 
+    }
   }
 
   updateProductCount(productId, amount) {
-    // ваш код
+    let findResult = this.cartItems.find(item => productId === item.product.id);
+    if (findResult) {
+      if(amount > 0) {
+        findResult.count++;
+        this.onProductUpdate(findResult);
+      }
+      else if(amount < 0) {
+        findResult.count--;
+        this.onProductUpdate(findResult);
+        if(findResult.count <= 0) {
+          this.cartItems.splice(this.cartItems.findIndex(item => productId === item.product.id)); 
+        }
+      }
+    }
   }
 
   isEmpty() {
-    // ваш код
+    if (this.cartItems.length === 0) return true;
+    else return false;
   }
 
   getTotalCount() {
-    // ваш код
+    let resultSum = 0;
+    this.cartItems.forEach((item) => {
+      resultSum += item.count;
+    });
+    return resultSum;
   }
 
   getTotalPrice() {
-    // ваш код
+    let resultPrice = 0;
+    this.cartItems.forEach((item) => {
+      resultPrice += item.product.price * item.count;
+    });
+    return resultPrice;
   }
 
   onProductUpdate(cartItem) {
@@ -31,4 +72,3 @@ export default class Cart {
     this.cartIcon.update(this);
   }
 }
-
